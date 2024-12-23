1e42b0945b0097a2eb1c11a5f1f052e05291cf12
Possibleeee <bussinessdiscord@gmail.com>
2024-12-23 15:55:14 -0500

    Add files via upload

--- _a/ccc101/ccc_s1/2013_s1.py
+++ _a/ccc101/ccc_s1/2013_s1.py
@@ -1,2 +1,12 @@
 a = int(input())
 
+while True:
+    lazySolution = set()
+    c = 0
+    a += 1
+    for i in str(a):
+        lazySolution.add(i)
+        c += 1
+    if len(lazySolution) == c:
+        print(a)
+        break
